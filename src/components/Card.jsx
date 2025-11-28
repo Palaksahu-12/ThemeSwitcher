c8404b30992f78696f86c3308f37acf54bce5c7e
Palak Sahu <palaksahu2233@gmail.com>
2025-11-29 01:23:18 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card(){
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg w-74 h-90 object-cover" src="https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
@@ -77,5 +77,6 @@ function Card(){
     );
 
 }
+
 
 export default Card;
